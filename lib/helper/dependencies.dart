43d16535e607d9cbf2525f217280ad74922c4ab5
Te1ne <nq2019thaigiabao230504@gmail.com>
2025-05-14 13:05:10 +0700

    first commit

---
+++ lib/helper/dependencies.dart
@@ -0,0 +1,26 @@
+import 'package:food_delivery/API/api_client.dart';
+import 'package:food_delivery/controllers/cart_controller.dart';
+import 'package:food_delivery/controllers/popular_product_controller.dart';
+import 'package:food_delivery/controllers/recommended_product_controller.dart';
+import 'package:food_delivery/repository/cart_repo.dart';
+import 'package:food_delivery/repository/popular_product_repo.dart';
+import 'package:food_delivery/repository/recommended_product_repo.dart';
+import 'package:food_delivery/utils/app_constants.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> init() async{
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  Get.lazyPut(()=> sharedPreferences);
+  //api client
+  Get.lazyPut(()=> ApiClient(appBaseUrl:AppConstants.BASE_URL));
+  //repos
+  Get.lazyPut(()=>PopularProductRepo(apiClient: Get.find()));
+  Get.lazyPut(()=>RecommendedProductRepo(apiClient: Get.find()));
+  Get.lazyPut(()=>CartRepo(sharedPreferences:Get.find()));
+  //controllers
+  Get.lazyPut(()=>PopularProductController(popularProductRepo: Get.find()));
+  Get.lazyPut(()=>RecommendedProductController(recommendedProductRepo: Get.find()));
+  Get.lazyPut(()=>CartController(cartRepo: Get.find()));
+}
